292cdae203061c0fe221289fa6702e956a34a0d8
mozss <mozss1024@gmail.com>
2021-11-14 09:29:17 +0800

    Initial commit

---
+++ src/note/demo/TestArrayList.java
@@ -0,0 +1,13 @@
+package note.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestArrayList {
+	public static void main(String[] args){
+		List<String> list = new ArrayList<>();
+		
+		list.add("Good Job!");
+		list.add("You can do it");
+	}
+}
